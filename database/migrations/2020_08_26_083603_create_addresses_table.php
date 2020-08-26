2eb9e2cb381611a25a870f22e207296925fc1b9e
Rakesh Kumar <justdecodeme@hotmail.com>
2020-08-26 14:15:33 +0530

    extra entry added

--- database/migrations/2020_08_26_083603_create_addresses_table.php
+++ database/migrations/2020_08_26_083603_create_addresses_table.php
@@ -15,6 +15,7 @@ class CreateAddressesTable extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->usingned()->nullable();
             $table->string('name');
             $table->timestamps();
         });
